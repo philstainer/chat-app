4a145553bf59810891fa596d9c63297c403d4538
Phil Stainer <philstainer.dev@gmail.com>
2020-11-26 14:23:30 +0000

    feat: Add sendEmail and tests

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -5,3 +5,7 @@ export const USER_EMAIL_ALREADY = 'Email already registered';
 // Auth
 export const AUTH_LOGGED_IN_ERROR = 'You are already logged in';
 export const AUTH_LOGGED_OUT_ERROR = 'You must be logged in to do that';
+
+// Mail
+export const SEND_MAIL_ERROR = 'Error sending email';
+export const MAIL_VALIDATION_ERROR = 'Error validating email data';
