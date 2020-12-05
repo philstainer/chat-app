e677348bfdc998d84b9632edca2a89d3508e19af
Phil Stainer <philstainer.dev@gmail.com>
2020-12-05 21:08:43 +0000

    feat: Add httpLink and wsLink to client setup

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,36 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 export const cache = new InMemoryCache();
 
-export const client = new ApolloClient({
-  uri: process.env.REACT_APP_API_URI,
-  cache,
+const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST;
+
+const httpLink = new HttpLink({
+  uri: BACKEND_HOST,
   credentials: 'include',
+});
+
+const wsLink = new WebSocketLink({
+  uri: BACKEND_HOST.replace(/(https|http)/, 'ws'),
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache,
 });
