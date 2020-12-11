1a546dcce23199bb8b0230e6e2ea6be65eb021b7
Phil Stainer <philstainer.dev@gmail.com>
2020-12-11 22:27:50 +0000

    fix: Remove user verify middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ import cookieParser from 'cookie-parser';
 
 import { apolloServer } from './graphql/apolloServer';
 import { accessEnv } from './utils/accessEnv';
-import { getUserIdentity } from './utils/getUserIdentity';
 
 const app = express();
 
@@ -13,9 +12,6 @@ app.use(cookieParser(accessEnv('BACKEND_COOKIE_SECRET')));
 
 // Compress All Responses
 app.use(compression());
-
-// Populate req.
-app.use(getUserIdentity);
 
 const corsOptions = {
   origin: true,
